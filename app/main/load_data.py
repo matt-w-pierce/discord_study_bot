031832ab86b38a50a6bcaca606b9c3755f12a83f
matt-w-pierce <Mustangs18!>
2020-06-27 00:45:29 -0400

    Added function to convert the raw text files into CSV files and load them Changed the location of the data_files directory

--- app/main/load_data.py
+++ app/main/load_data.py
@@ -4,11 +4,76 @@ from .. import db
 from ..models import Question
 
 
-def load_question_csv(filename):
+def load_txt_file(txt_file):
+    """
+    Converts the text file to a CSV and loads the CSV file data into the database
+    :param txt_file: Name of the file to be converted and loaded
+    :return: Return value from load_csv_file function
+    """
+    base_path = 'data_files'
+
+    # Converts the text file to a CSV file
+    csv_file = txt_to_csv(txt_file, base_path)
+    data_len = load_csv_file(csv_file, base_path)
+    return data_len
+
+
+def txt_to_csv(in_file, base_path='data_files'):
+    """
+    Reads in a text file with '<!>' as row separators and '|' as column separators and converts to CSV file
+    :param base_path: Path to the data file directory
+    :param in_file: Name of the text file to read in
+    :return: CSV file name
+    """
+    in_path = os.path.join(base_path, in_file)
+
+    out_file = f'{os.path.splitext(in_file)[0]}.csv'
+    out_path = os.path.join('app', 'data_files', out_file)
+
+    txt_in = open(in_path, 'rt')
+    txt_out = open(out_path, 'wt')
+    txt_out.write('"prompt","answer"\n')
+
+    lines = txt_in.readlines()
+    # print(f'Number of lines: {len(lines)}')
+    for i, line in enumerate(lines):
+
+        # Removing any instances of double-quotes with single quotes
+        line = line.replace('"', "'")
+
+        # The first line needs a leading double quote
+        if i == 0:
+            line = '"' + line
+
+        # Replacing custom delimiters with quote bound CSV delimiters
+        line = line.replace('|', '","')
+        line = line.replace('<!>', '"\n"')
+
+        # If it is the last line, then we don't need the closing quote, which is the last character
+        if i == (len(lines) - 1):
+            line = line[:-1]
+
+        # Writing the line to the output file
+        txt_out.write(line)
+
+    # Closing input and output files
+    txt_in.close()
+    txt_out.close()
+
+    return out_file
+
+
+def load_csv_file(csv_file, base_path='data_files'):
+    """
+    Reads in the provided CSV file and stores the data in the MySQL database
+    :param base_path: Path to the data file directory
+    :param csv_file: Name of the text file that should be read in
+    :return: Length of the data loaded
+    """
 
     # Locates the CSV file, loads into a dataframe and then converts into a list of dicts
-    df = pd.read_csv(os.path.join('app', 'data_files', filename))
-    set_name = filename[:-4]
+    df = pd.read_csv(os.path.join(base_path, csv_file))
+    set_name = csv_file[:-4]
     df['set_name'] = set_name
     data = df.T.to_dict().values()
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from . import main
-from .load_data import load_question_csv
+from .load_data import load_txt_file, txt_to_csv, load_csv_file
 from flask import render_template, request
 
 
@@ -11,11 +11,37 @@ def index():
 @main.route('/load_data', methods=['GET'])
 def load_new_data():
 
-    # Retreives the filename from the request
+    # Retrieves the filename from the request
     filename = request.args.get('filename', type=str)
     if filename is None:
         return render_template('500.html'), 500
 
     else:
-        num_rows = load_question_csv(filename)
+        num_rows = load_txt_file(filename)
+        return f'Loaded {filename} successfully. Loaded {num_rows} questions into the database.'
+
+
+@main.route('/txt_to_csv', methods=['GET'])
+def convert_txt_to_csv():
+
+    # Retrieves the filename from the request
+    filename = request.args.get('filename', type=str)
+    if filename is None:
+        return render_template('500.html'), 500
+
+    else:
+        out_file = txt_to_csv(filename)
+        return f'Converted {filename} to {out_file} successfully'
+
+
+@main.route('/load_existing_csv', methods=['GET'])
+def load_new_data():
+
+    # Retrieves the filename from the request
+    filename = request.args.get('filename', type=str)
+    if filename is None:
+        return render_template('500.html'), 500
+
+    else:
+        num_rows = load_csv_file(filename)
         return f'Loaded {filename} successfully. Loaded {num_rows} questions into the database.'
