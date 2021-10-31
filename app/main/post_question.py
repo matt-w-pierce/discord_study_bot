7845ae9ffeef38cdcf3ab2d387d7a607f8805c17
matthew-pierce <matthew@3mrgntdigital.com>
2021-10-31 17:20:59 -0400

    Updating the cron jobs for new channels Updating the name of a file

--- app/main/post_question.py
+++ app/main/post_question.py
@@ -5,17 +5,25 @@ from .. import scheduler, db
 from ..models import Question
 
 test_url = 'https://discordapp.com/api/webhooks/702270009887555657/mytFno51Sq4fx0wvKyr22S8EwpSOFmLxyEUasKLoOnGoKurhWduq7uqRDpHBlNtMtL1C'
-ap_url = 'https://discordapp.com/api/webhooks/702887067625062451/ERkWP2WnZ9VHrXvi0jwyDQIMur_5teoU4kVESBh863wGffsDeQMc0Sqf2sw8mixFsrmm'
-pharm_url = 'https://discordapp.com/api/webhooks/703048422231375903/Q3dq_CWinB7KGGbCI9fNRtw6S2VNDqf404gebUQVv4Idw_M60kRTq-1hzhGMQs5n3OYN'
-anat_url = 'https://discord.com/api/webhooks/703048779632476240/tRHSEwV9B_BcopXa69-vImpzvVSavBLlvFNcHiO_S_2uRd2jalSOL0MHz2zFUMuYrZFt'
+unit_1_url = 'https://discord.com/api/webhooks/904471895410360321/IiQ49M-uSKktqu33W5SrRARcu8AZR7Usd4pVjdIX4sGNKW17TKF_nTnSIJN2LRdpva5x'
+unit_2_url = 'https://discord.com/api/webhooks/904472615740129290/3QeLZC8lOfeyioNQFhuZXo5XgIL3tpmZNnqVhtU2HeE-_VVYEJ4GYcXNLhjXgMymBzWk'
+unit_3_url = 'https://discord.com/api/webhooks/904472942744834148/lsw253GREGR2Qug0tHNF2XYpida-6XdOHJUe5pt3QbROopfNho73_DMXbSUj6qkm6M8P'
+unit_4_url = 'https://discord.com/api/webhooks/904473171506364458/nYFgwVpcNh2LZ9Dz4aBwTJIldrP4AiL3izOkhqf6SKWvswZqq3hRFuDWvRXMAArASGhO'
+unit_5_url = 'https://discord.com/api/webhooks/904473813524295720/mrAlmUSzwGTi1KgCv0sZ34rvrFZy34eQd-erWYM8UKSwBmwwugc2wnYJaTPVjpWvsZEF'
+unit_6_url = 'https://discord.com/api/webhooks/904473885771182140/1Fe77feGiaV16Joiu6mzXyvpOdP3yjLLKTE03xXVbqoJvamF18ReAzdM1piBsBNPCzIA'
+unit_7_url = 'https://discord.com/api/webhooks/904473967199387680/ewK30TDffuOGGr2WnAWMKMqdmhy-v1jxe1QtABDONdGPS-U6DWfk3_Ud9g4MxgSuJkEV'
+unit_8_url = 'https://discord.com/api/webhooks/904474047432245298/dZ1LUn1l4RAKqA4VoxeOHjwkHouCXnOcdtCXjk6XBRJ19skeSv1FJAcnL9S_m2rFxJOP'
+unit_9_url = 'https://discord.com/api/webhooks/904474123097481268/_MVE8F8RiSkVDkn-EZT42FEsarugBc3jk4BYMD5pyA5kCuyfaFI71N53mvWqqRD5EZNW'
+unit_10_url = 'https://discord.com/api/webhooks/904474198645293086/0IZaH3_S52fBidxo2fV4ZzUE_uqgcTQtKZ0NkeJI0W3urjhuT-fo97uZGpL9SOEY8U4_'
+unit_11_url = 'https://discord.com/api/webhooks/904474270304960564/GI7_8uOQXrZqayCOPzf-sMsTMxXtIozF8bKCypbe2M-t-t61eleEQA5J1GICUdYEehtV'
+unit_12_url = 'https://discord.com/api/webhooks/904474343713697812/G4NrLfhWbJCp4xEm5K4H_inxTqgVTt8qjqnnSYkVIeC53cJujR0HYhpDhY3LiK23qiUi'
 
-
-@scheduler.task(trigger='cron', id='post_ap_hour', hour='8-20/2', args=[['Anesthesia Principles I', 'NA2 Exam 1'], None, ap_url])
-@scheduler.task(trigger='cron', id='post_pharm_hour', hour='8-20/2', args=[['Nagelhout Pharmacology II'], None, pharm_url])
-@scheduler.task(trigger='cron', id='post_anat_hour', hour='8-20/2', args=[['Intro and Back Anatomy', 'Head and Neck', 'Thorax, Abdomen and Pelvis', 'Upper and Lower Limb'], [0.07, 0.07, 0.06, 0.8], anat_url])
-
-
-
+@scheduler.task(trigger='cron', id='unit_1', hour='8-20/2', args=[['Airway Anatomy', 'Respiratory Physiology', 'Airway Management'], None, unit_1_url])
+@scheduler.task(trigger='cron', id='unit_2', hour='8-20/2', args=[['ANS'], None, unit_2_url])
+@scheduler.task(trigger='cron', id='unit_3', hour='8-20/2', args=[['Cardiac A&P', 'Valvular Heart Disease', 'Cardiac Patho'], None, unit_3_url])
+@scheduler.task(trigger='cron', id='unit_4', hour='8-20/2', args=[['Pharmacokinetics', 'Pharmacodynamics', 'IV Anesthetics', 'Volatile Anesthetics I', 'Volatile Anesthetics II'], None, unit_4_url])
+@scheduler.task(trigger='cron', id='unit_5', hour='8-20/2', args=[['Local Anesthetics', 'Neuromuscular Blockers', 'NMBD Reversal', 'Opioid Agonists and Antagonists'], None, unit_5_url])
+@scheduler.task(trigger='cron', id='unit_6', hour='8-20/2', args=[['AGM', 'Breathing Circuit', 'Monitoring I (Respiratory)', 'Monitoring II (HD)', 'Monitoring III (CV)', 'Monitoring IV (Misc)'], None, unit_6_url])
 def post_question(set_name_l, set_weights, hook_url):
     with scheduler.app.app_context():
 
